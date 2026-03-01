677b4e9b6207a44f0eb447f697fdcb23ab0a6703
Anh Dang <anhtdang92@gmail.com>
2026-02-28 23:59:54 -0600

    Revert film grain disabler to simple always-on (remove non-working toggle)

--- No Film Grain-18-1-0-0-1772264739/reframework/autorun/disable_film_grain.lua
+++ No Film Grain-18-1-0-0-1772264739/reframework/autorun/disable_film_grain.lua
@@ -2,42 +2,20 @@
 -- > Place in: reframework/autorun/disable_film_grain.lua
 
 local log_tag = "[FilmGrainDisabler]"
-local disable_active = true
 
--- Load saved setting
-local saved = json.load_file("film_grain_disabler.json")
-if saved ~= nil and saved.disable_active ~= nil then
-    disable_active = saved.disable_active
-end
-
-local function apply_film_grain()
+local function disable_film_grain()
     local mgr = sdk.get_managed_singleton("app.RenderingManager")
-    if not mgr then return end
-
-    if disable_active then
+    if mgr and mgr:call("get__IsFilmGrainCustomFilterEnable") then
         mgr:call("set__IsFilmGrainCustomFilterEnable", false)
-    else
-        mgr:call("set__IsFilmGrainCustomFilterEnable", true)
     end
 end
 
 re.on_frame(function()
-    apply_film_grain()
-end)
-
-re.on_config_save(function()
-    json.dump_file("film_grain_disabler.json", { disable_active = disable_active })
+    disable_film_grain()
 end)
 
 re.on_draw_ui(function()
     if imgui.tree_node("Film Grain Disabler") then
-        local changed
-        changed, disable_active = imgui.checkbox("Disable Film Grain", disable_active)
-        if changed then
-            apply_film_grain()
-            json.dump_file("film_grain_disabler.json", { disable_active = disable_active })
-        end
-
         local mgr = sdk.get_managed_singleton("app.RenderingManager")
         if mgr then
             local val = mgr:call("get__IsFilmGrainCustomFilterEnable")
@@ -49,4 +27,4 @@ re.on_draw_ui(function()
     end
 end)
 
-log.info(log_tag .. " Script loaded. Film grain disabler " .. (disable_active and "active" or "inactive"))
+log.info(log_tag .. " Script loaded. Film grain disable active.")
